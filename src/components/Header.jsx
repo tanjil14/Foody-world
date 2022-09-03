ebe5fc70c1954e62e76fb325a65b3d5c70f36f50
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-09-03 23:56:35 +0600

    addidng logout design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { motion } from "framer-motion";
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket,MdLogout,MdAdd } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
@@ -12,14 +12,16 @@ const Header = () => {
   const provider = new GoogleAuthProvider();
   const [{ user }, dispatch] = useStateValue();
   const login = async () => {
-    const {
-      user: { accessToken, providerData },
-    } = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0],
-    });
-    localStorage.setItem("user", JSON.stringify(providerData[0]));
+    if (!user) {
+      const {
+        user: { accessToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
   };
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
@@ -58,6 +60,10 @@ const Header = () => {
               alt="userProfile"
               onClick={login}
             />
+            <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd/></p>
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout<MdLogout/></p>
+            </div>
           </div>
         </div>
       </div>
--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,3 +1,6 @@
-export const initialState={
-    user:null
-}
+import { fetchUser } from "../utils/fetchLocalStorageData";
+
+const userInfo = fetchUser();
+export const initialState = {
+  user: userInfo,
+};
