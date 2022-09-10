d910332247c59fb7105d94b276334e9c85830c09
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-09-10 21:25:32 +0600

    complete home

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,7 +1,9 @@
 import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
 const HomeContainer = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
       <div className="py-2 flex flex-1 flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
@@ -32,9 +34,41 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBg}
+          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-bg"
+        />
 
-      <div className="py-2 bg-blue-400 flex-1"></div>
-    </div>
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-16 py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
