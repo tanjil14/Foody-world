0486d3aba1c6f5d0901e6f29d45a83cb138963ad
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-09-14 14:09:29 +0600

    item part 1 done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
     });
   };
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
@@ -87,7 +87,10 @@ const Header = () => {
               >
                 {user && user.email === "tanjil14@cse.pstu.ac.bd" && (
                   <Link to={"/createItem"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setIsMenu(false)}
+                    >
                       New Item <MdAdd />
                     </p>
                   </Link>
@@ -133,7 +136,7 @@ const Header = () => {
             >
               {user && user.email === "tanjil14@cse.pstu.ac.bd" && (
                 <Link to={"/createItem"}>
-                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base" onClick={() => setIsMenu(false)}>
                     New Item <MdAdd />
                   </p>
                 </Link>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as CreateContainer } from "./CreateContainer.jsx";
 export { default as Header } from "./Header.jsx";
 export { default as HomeContainer } from "./HomeContainer.jsx";
 export { default as MainContainer } from "./MainContainer.jsx";
+export { default as Loader } from "./Loader.jsx";
